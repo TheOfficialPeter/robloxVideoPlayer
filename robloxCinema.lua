8cdd06f6fee56b85787c139b228b1c17d202907e
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2022-12-29 19:04:39 +0000

    second update. Fixed module + change a few methods

--- robloxCinema.lua
+++ robloxCinema.lua
@@ -1,3 +1,5 @@
+local lightingModule = require(robloxCinemaLighting)
+
 local screen = workspace["MainScreen"]
 
 local screenWidth = 1000
@@ -6,11 +8,39 @@ local screenHeight = 1000
 -- The list of all pixels (already sorted)
 local pixels = {}
 
-function updatePixels(pixelMap)
+-- color totals for each quadrant
+local quadrant1 = {0,0,0}
+local quadrant2 = {0,0,0}
+local quadrant3 = {0,0,0}
+local quadrant4 = {0,0,0}
+
+function updatePixels(pixelMap, screenWidth, screenHeight)
     -- update pixels with the pixelMap
     -- screen updates vertically from left to right. It updates in columns
     for i,pixel in pairs(pixels) do
-        
+        if pixel.Position.X < screenWidth/2 and pixel.Position.Y < screenHeight/2 then
+            -- quadrant 1
+            quadrant1[1] = quadrant1[1] + pixel.Color.R
+            quadrant1[2] = quadrant1[2] + pixel.Color.G
+            quadrant1[3] = quadrant1[3] + pixel.Color.B
+        elseif pixel.Position.X > screenWidth/2 and pixel.Position.Y < screenHeight/2 then
+            -- quadrant 2
+            quadrant2[1] = quadrant2[1] + pixel.Color.R
+            quadrant2[2] = quadrant2[2] + pixel.Color.G
+            quadrant2[3] = quadrant2[3] + pixel.Color.B
+        elseif pixel.Position.X < screenWidth/2 and pixel.Position.Y > screenHeight/2 then
+            -- quadrant 3
+            quadrant3[1] = quadrant3[1] + pixel.Color.R
+            quadrant3[2] = quadrant3[2] + pixel.Color.G
+            quadrant3[3] = quadrant3[3] + pixel.Color.B
+        elseif pixel.Position.X > screenWidth/2 and pixel.Position.Y > screenHeight/2 then
+            -- quadrant 4
+            quadrant4[1] = quadrant4[1] + pixel.Color.R
+            quadrant4[2] = quadrant4[2] + pixel.Color.G
+            quadrant4[3] = quadrant4[3] + pixel.Color.B
+        end
+
+        -- Change the pixel's color
     end
 end
 
@@ -19,6 +49,7 @@ for i = 1,screenWidth,1 do
         local pixel = Instance.new("Frame", screen)
         pixel.Name = i.."-"..i2
         pixel.Size = UDim2.new(0,1,0,1)
+        pixel.Position = UDim2.fromOffset(i,i2)
         table.insert(pixels, #pixels+1, pixel)
     end
 end
--- robloxCinemaLighting.lua
+++ robloxCinemaLighting.lua
@@ -1,64 +1,16 @@
 local module = {}
 
-local quadrant1 = {}
-local quadrant2 = {}
-local quadrant3 = {}
-local quadrant4 = {}
+module.lights = {}
 
-local screenWidth = 1000
-local screenHeight = 1000
-
-table.insert(quadrant1, #quadrant1+1, {0,0})
-table.insert(quadrant1, #quadrant1+1, {screenWidth/2, screenHeight/2})
-
-table.insert(quadrant2, #quadrant2+1, {screenWidth/2, 0})
-table.insert(quadrant2, #quadrant2+1, {screenWidth, screenHeight/2})
-
-table.insert(quadrant3, #quadrant3+1, {0, screenHeight/2})
-table.insert(quadrant3, #quadrant3+1, {screenWidth/2, screenHeight})
-
-table.insert(quadrant4, #quadrant4+1, {screenWidth/2, screenHeight/2})
-table.insert(quadrant4, #quadrant4+1, {screenWidth, screenHeight})
-
-function getFirstColor()
-    local total = 0
-    local totalR = 0
-    local totalG = 0
-    local totalB = 0
-    
-    return {totalR/total, totalG/total, totalB/total}
+function module.updateLighting(averageColorArray)
+    -- updates the screen lighting effect
+    for i,v in pairs(module.lights) do
+        v.Color = averageColorArray[i]
+    end
 end
 
-function getSecondColor()
-    local total = 0
-    local totalR = 0
-    local totalG = 0
-    local totalB = 0
-    
-    return {totalR/total, totalG/total, totalB/total}
-end
-
-function getThirdColor()
-    local total = 0
-    local totalR = 0
-    local totalG = 0
-    local totalB = 0
-    
-    return {totalR/total, totalG/total, totalB/total}
-end
-
-function getFourthColor()
-    local total = 0
-    local totalR = 0
-    local totalG = 0
-    local totalB = 0
-    
-    return {totalR/total, totalG/total, totalB/total}
-end
-
-local colorFuncs = {getFirstColor, getSecondColor, getThirdColor, getFourthColor}
-
-function module.loadCinemaLighting(mainScreenPart, screenWidth, screenHeight)
+function module.loadCinemaLighting(mainScreenPart, screenWidth, screenHeight, averageColorArray)
+    -- Average Color Array includes the average color for each corner of the screen. Used to set the VIBE of the cinema by adding realistic lighting. Casting rays was my first option, but too performance heavy.
     -- Make sure that the screens lookvector is facing the crowd
     -- Make sure that the screen is placed in the x-axis and not z-axis. It wil cause the lights to be place in the wrong direction
     local lightDistanceFromScreen = 5
@@ -66,7 +18,6 @@ function module.loadCinemaLighting(mainScreenPart, screenWidth, screenHeight)
 
     for i = 1,4,1 do
         local newLight = Instance.new("PointLight", workspace)
-        newLight.Color =  Color3.new(colorFuncs[i])
 
         if i == 1 then
             newLight.CFrame = lightingPosition.CFrame - Vector3.new(screenWidth/2, screenHeight/2, 0)
