427d881dac612cf458738fab9449a0c2a1b796a6
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2023-01-05 12:39:47 +0000

    update

--- convert.py
+++ convert.py
@@ -9,10 +9,10 @@ nparr = np.array(img)
 
 count = 0
 
-for i in range(0,499):
+for i in range(0,49):
     count += 1
     print(count)
-    for i2 in range(0,999):
+    for i2 in range(0,99):
         file.write("[" + str(nparr[i][i2][0]).replace(" ","") + "," + str(nparr[i][i2][1]).replace(" ","") + "," + str(nparr[i][i2][2]).replace(" ","") + "]" + ";")
     file.write(".")
 
