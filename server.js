1ececcb3768dd1cf154e83eeb761118eb3eb12d3
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2023-01-30 15:31:41 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const https = require('axios')
+const { default: axios } = require('axios')
+
+const app = express()
+const port = 8080
+var pixelMap = "";
+
+app.get('/get', (req, res) => {
+    res.send(pixelMap);
+})
+
+app.post('/post', (req, res) => {
+    if (req.query.code && req.query.code == "theofficialpeterisreallyreallcool1100"){
+        // save the latest pixelmap
+        console.log(req.body);
+        pixelMap = JSON.parse(req.body);
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
